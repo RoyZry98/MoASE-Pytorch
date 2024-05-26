ddf88219db3658cca0e1b913f901fb898ce5bede
Rongyu Zhang <98035495+RoyZry98@users.noreply.github.com>
2024-05-26 16:16:35 +0800

    Update run_cifar10.sh

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -1,6 +1,3 @@
-#source /cluster/home/qwang/miniconda3/etc/profile.d/conda.sh
-# conda deactivate
-# conda activate cotta 
 CUDA_VISIBLE_DEVICES=6 python cifar10c_vit.py --cfg cfgs/cifar10/vit.yaml
 # CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/source.yaml
 # CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/norm.yaml
